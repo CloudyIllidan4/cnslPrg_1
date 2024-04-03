3992d5b524cfa7eec82de0d567d572f7c6e52ee0
chertkov <chertkov@cate.ru>
2024-04-03 17:56:56 +0300

    Добавлена поддержка работы с массивами (чтение, замена по индексам, перезапись значений)

--- cnslPrg_1.cpp
+++ cnslPrg_1.cpp
@@ -21,85 +21,9 @@ int main()
     flIn.close();
 
     // Преобразование содержимого
-    
     //size_t i{ dataIn.size() };
-
     json dataOut = converterFunc(dataIn); // применяем нашу рекурсивную функцию конвертации
-    
     //std::cout << dataOut << std::endl;
-    
-    /*
-    //std::cout << dataIn.is_object() << std::endl;
-    //json::const_reverse_iterator iter1{ dataIn.crbegin() };
-    json::const_iterator iter2{ dataIn.begin() };
-    //std::cout << *iter1 << std::endl;
-    for (auto x{ iter2 }; x != dataIn.end(); x++)
-        //json::value_t::object;
-
-        //for (iter1; iter1 != iter2; iter1++)
-    {
-        /*
-        std::cout << x.key() << '\n'
-                << x.value() << std::endl;
-                
-        json valueJSON = x.value();
-        //json::array_t tempBuff{};
-        json::string_t stringValue{};
-        //auto valSize{ valueJSON.size() }; // указывает на количество значений, а не на число символов в них
-        //std::cout << std::boolalpha;
-        //std::cout << valueJSON.is_number_integer() << std::endl;
-        //std::is_same< valueJSON, json::number_float_t>::value;
-        
-        if (valueJSON.is_number_integer()) // если тип значения JSON такой, то меняем значение вот так ->
-        //if (typeid valueJSON === json:object_t) 
-        {
-            valueJSON.operator=(333);
-            //std::cout << valueJSON << std::endl;
-        };
-        if (valueJSON.is_number_float())
-        {
-            //std::cout << valueJSON << std::endl;
-        };
-        if (valueJSON.is_string())
-        {
-
-            std::string tempString{ valueJSON };
-            
-
-            for (auto k{0}; k <= tempString.size(); k++)
-            {
-                //stringValue.operator[]() = tempString[k];
-                //tempValue[k].operator=(valueJSON.at(k));                       
-            };
-            std::cout << stringValue << std::endl;
-            /*
-            for (auto k{ 0 }; k <= valSize; k++)
-            {
-                valueJSON[k].operator=(tempValue[valSize - k]);
-            };
-            
-            //std::cout << valueJSON << std::endl;
-        };
-        if (valueJSON.is_array())
-        {
-            /*
-            json::array_t tempBuffer{};
-            auto arrSize{ valueJSON.size() };
-            for (auto k{ 0 }; k <= arrSize; k++)
-            {
-                tempBuffer[arrSize - k] = valueJSON[k];
-            }
-            std::cout << tempBuffer << arrSize << std::endl;
-            
-        };
-        dataOut[x.key()].operator=(stringValue);
-        //dataOut.update(x);
-        //dataOut[x.key()] = x.value();
-        
-        //dataOut[i - 1][i] = x.value();
-        
-    };
-    */
 
     // Запись в новый JSON
     std::string pathOutExample = "./OutJSON/ClientConfig_.json";
--- cnslPrg_1.h
+++ cnslPrg_1.h
@@ -7,9 +7,7 @@ using json = nlohmann::json;
 
 auto is_primitive(json& jsonIn)
 {
-
 	//if (jsonIn.type() == json::value_t::boolean)
-
 	if (jsonIn.is_boolean())
 	{
 		jsonIn = !jsonIn;
@@ -47,7 +45,6 @@ json converterFunc(json& jsonIn)
 {
 	try
 	{
-
 		//int n{ number_call }; // счетчик примитивов
 		/*
 		if () // ?был сделан последний вызов из стека?
@@ -56,121 +53,42 @@ json converterFunc(json& jsonIn)
 		};
 		*/
 		//std::cout << jsonIn << std::endl;
-
 		if (jsonIn.is_object())
 		{
-
 			/*
 			json::iterator iter{ jsonIn.begin() };
 			for (auto x{ iter }; x != jsonIn.end(); x++)
 			{
 			*/
-			//json jsonResult{};
-			//json jsonTemp{};
-
 			for (auto& x : jsonIn.items())
 			{
-
-				//jsonTemp[x.key()] = x.value();
-				//std::cout << x.value().type_name() << std::endl;
-
-				std::cout << x.key() << " - " << jsonIn[x.key()] << " - 1st out message - " << *jsonIn[x.key()].type_name() << std::endl;
+				//std::cout << x.key() << " - " << jsonIn[x.key()] << " - 1st out message - " << *jsonIn[x.key()].type_name() << std::endl;
 				jsonIn[x.key()] = converterFunc(x.value());
-				//std::cout << " - " << jsonIn << " - 2 - " << std::endl;
-
-				/*
-				if (jsonTemp[x.key()].is_boolean()) {
-					jsonTemp[x.key()] = !jsonTemp[x.key()];
-
-					std::cout << jsonTemp[x.key()] << " - jsonTemp - " << jsonTemp[x.key()].type_name() << std::endl;
-				}
-				else
-				{
-					jsonResult[x.key()], number_call = converterFunc(jsonTemp[x.key()], number_call);
-					std::cout << jsonResult[x.key()] << " - jsonResult - " << jsonResult[x.key()].type_name() << std::endl;
-
-				}
-				*/
-				//	jsonResult[x.key()], number_call = converterFunc(jsonTemp[x.key()], number_call);
-				////std::cout << std::boolalpha << jsonResult.contains(x.key()) << std::endl;
-				//std::cout << jsonResult[x.key()] << " - jsonResult - " << jsonResult[x.key()].type_name() << std::endl;
-				////return converterFunc(jsonTemp);
-				//return jsonResult;
-				//jsonResult[x.key()].operator=(converterFunc(jsonTemp));
+				//std::cout << " - " << jsonIn << " - 2nd out message - " << std::endl;
+				////std::cout << std::boolalpha << jsonIn.contains(x.key()) << std::endl;
 				//std::cout << x.key() << std::endl;	
-
 			};
 			//std::cout << jsonIn << " -  - " << *jsonIn.type_name() << std::endl;
 			return jsonIn;
-
-			//json jsonTemp = [x.key(), x.value()];
-			//json jsonTemp = json::parse([x.key(), x.value()]);
 			//return converterFunc(jsonTemp);
 		}
-		/*
-		/*
-		else if (jsonIn.is_number())
-		{
-			if (jsonIn.is_number_integer())
-			{
-				number_call++;
-				auto limit = jsonIn.size() - 1;
-				json jsonOut{};
-				std::cout << limit << std::endl;
-
-				for (int j{}; j <= (limit); j++)
-				{
-					jsonOut[j] = jsonIn[limit - j];
-				};
-				return jsonOut;
-			};
-			if (jsonIn.is_number_float())
-			{
-				number_call++;
-				return jsonIn;
-			};
-		}
-		*/
-		/*
-		else if (jsonIn.is_string())
-		{
-			number_call++;
-			auto tempString = jsonIn.template get<json::string_t>();
-			std::reverse(tempString.begin(), tempString.end());
-			/*
-			auto limit = jsonIn.size() - 1;
-			json jsonOut{};
-			for (int j{}; j <= (limit); j++)
-				{
-					jsonOut[j] = jsonIn[limit - j];
-				};
-			
-			return tempString, number_call;
-		}
-		*/
-		/*
 		else if (jsonIn.is_array())
 		{
-			number_call++;
-			//std::cout << limit << std::endl;
-			//auto jsonOut = jsonIn.operator[](0);
-			//std::cout << (jsonIn.type() == json::value_t::array) << std::endl;
-			auto limit = jsonIn.size() - 1;
-			json jsonOut{};
-			for (int j{}; j <= (limit); j++)
+			auto maxIndex = (jsonIn.size() == 0) ? 0 : (jsonIn.size() - 1);
+			//std::cout << " - array size - " << maxIndex << std::endl;
+			json tempArray{};
+			for (int j{}; j <= (maxIndex); j++)
 			{
-				jsonOut[j] = jsonIn[limit - j];
+				tempArray[j] = converterFunc(jsonIn[maxIndex - j]);
+				//std::cout << " - array value - " << tempArray[j] << " - type - " << *tempArray[j].type_name() << std::endl;
 			};
-			return jsonOut, number_call;
+			jsonIn = tempArray;
+			return jsonIn;
 		}
-		*/
 		else
 		{
-
 			//number_call++;
 			//std::cout << "the type of the value N " << number_call << ": " << jsonIn << " - " << jsonIn.type_name() << std::endl;
-			//std::cout << jsonIn.type_name() << std::endl;
-
 			return is_primitive(jsonIn);
 		};
 	}
